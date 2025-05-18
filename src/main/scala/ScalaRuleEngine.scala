28657c6b9ba27bbc31f91495c8582d09f22d011a
yomnaafifi <yomnaaafifi162@gmail.com>
2025-05-18 20:40:53 +0300

    base logic

--- src/main/scala/ScalaRuleEngine.scala
+++ src/main/scala/ScalaRuleEngine.scala
@@ -5,8 +5,8 @@ import java.time.temporal.ChronoUnit
 
 object ScalaRuleEngine extends App {
   val lines: List[String] = Source.fromFile("src/main/resources/TRX1000.csv").getLines().drop(1).toList
-  case class Order (transaction_date: LocalDate, product_name: String,
-                    expiry_date: LocalDate, quantity: Int, unit_price: Double,  channel: String, payment_method: String)
+  case class Order (transaction_date: LocalDate, product_name: String,expiry_date: LocalDate,
+                    quantity: Int, unit_price: Double,  channel: String, payment_method: String, discount: Option[Double] = None, finalprice: Option[Double] = None)
 
   def maptoOrder (line:String): Order = {
     val Array(timestamp, productName, expiryDate, quantity, unitPrice, channel, paymentMethod) = line.split(",")
@@ -17,7 +17,7 @@ object ScalaRuleEngine extends App {
     Order(transactionDate, productName, expiry, quantity.toInt, unitPrice.toDouble, channel, paymentMethod)
   }
 
-  lines.map(maptoOrder).foreach(println)
+//  lines.map(maptoOrder).foreach(println)
 
   //Qualifiers
   def is_lessthan30 (order: Order): Boolean = ChronoUnit.DAYS.between(order.transaction_date, order.expiry_date) < 30
@@ -31,19 +31,39 @@ object ScalaRuleEngine extends App {
     if (days >=1) (30-days)/100.0 else 0.0
   }
 
-  def calc_cheeseorwine(order:Order): Double = order.product_name match {
-      case "Cheese" => 0.10
-      case "Wine" => 0.05
-    }
-
+  //extract only the name to match
+  def calc_cheeseorwine(order:Order): Double =  order.product_name match {
+    case name if name.startsWith("Cheese") => 0.10
+    case name if name.startsWith("Wine")   => 0.05
+  }
   def calc_soldon23(order:Order): Double = 0.5
-
   def calc_5ofthesame(order:Order): Double = order.quantity match {
       case q if q >= 15 => 0.10
       case q if q >= 10 => 0.07
       case q if q >= 6  => 0.05
     }
 
+//applying rules to orders
+  val rules: List[(Order => Boolean, Order => Double)] = List(
+    (is_lessthan30, calc_lessthan30),
+    (is_cheeseorwine, calc_cheeseorwine),
+    (is_soldon23, calc_soldon23),
+    (is_5ofthesame, calc_5ofthesame)
 
+  )
 
+  //a function that calculates discount and the final price then add them to the order object
+  def getFinalPrice (order: Order, rules:List[(Order => Boolean, Order => Double)]): Order= {
+    val discounts = rules.filter(_._1(order)).map(_._2(order)).sorted.reverse.take(2)
+    val discount = if (discounts.nonEmpty) discounts.sum / discounts.length else 0.0
+    val finalprice = order.unit_price * (1 - discount)
+
+    order.copy(
+      discount = Some(discount),
+      finalprice = Some(finalprice)
+    )
+  }
+
+  lines.map(maptoOrder).map(x => getFinalPrice(x, rules)).foreach(println)
 }
+
